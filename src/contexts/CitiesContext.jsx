d8f935d0e8f3b53693341fe5385066a51d4dad7a
Colin Huber <cbhuber@gmail.com>
2023-07-05 18:45:07 -0600

    refactor: citiescontext hook.

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,8 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "http://localhost:8000/cities";
+// const BASE_URL = 'http://localhost:8000/cities'
 
 const CitiesContext = createContext();
 
