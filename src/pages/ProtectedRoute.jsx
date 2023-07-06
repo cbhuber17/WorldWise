f6a3a4da6324a4b46817643f7223d6e99b9442d3
Colin Huber <cbhuber@gmail.com>
2023-07-05 22:38:17 -0600

    feat: protect routes in the app pages.

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+
+/* eslint react/prop-types: 0 */
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  // Not authenticated, just put back to home page
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return isAuthenticated ? children : null;
+}
+
+export default ProtectedRoute;
