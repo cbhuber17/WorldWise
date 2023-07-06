5849349e889128439c3039fae218b69476fc405b
Colin Huber <cbhuber@gmail.com>
2023-07-05 21:51:06 -0600

    fix: json-server API URL.

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,8 +6,8 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:8000/";
-// const BASE_URL = 'http://localhost:8000/'
+const BASE_URL = "http://localhost:8000";
+// const BASE_URL = 'http://localhost:8000'
 
 const CitiesContext = createContext();
 
