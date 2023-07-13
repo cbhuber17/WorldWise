7b24c23e99fd5d3a464964588247b198c2314044
Colin Huber <cbhuber@gmail.com>
2023-07-13 13:46:16 -0600

    fix: image rendering.

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -19,7 +19,10 @@ export default function Product() {
             voluptatem iste.
           </p>
         </div>
-        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+        <img
+          src="/src/img-2.jpg"
+          alt="overview of a large city with skyscrapers"
+        />
       </section>
     </main>
   );
