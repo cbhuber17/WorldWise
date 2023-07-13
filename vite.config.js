b0502e3231e9ba4ab28139e2702f6d04ed3d5c0c
Colin Huber <cbhuber@gmail.com>
2023-07-13 13:13:28 -0600

    env: gh pages.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/WorldWise",
+  base: "/WorldWise/",
   plugins: [react(), eslint()],
 });
