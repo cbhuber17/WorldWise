9f9e1fe4e24384ab899b6a746517a6bee0d82e5f
Colin Huber <cbhuber@gmail.com>
2023-10-23 09:11:09 -0400

    feat: update user text.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,8 +14,8 @@ export default function Homepage() {
         </h1>
         <h2>
           A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          of. Never forget your wonderful experiences, and show your friends &
+          family how you have wandered the world.
         </h2>
         <Link to="/login" className="cta">
           Start tracking now
