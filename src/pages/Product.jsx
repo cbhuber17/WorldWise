4be0fd6f7849b49514a3500ab0292f112cfb655e
Colin Huber <cbhuber@gmail.com>
2023-11-02 09:52:27 -0600

    feat: product page hyperlinks to providers.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,29 @@
 import PageNav from "../components/PageNav";
 import styles from "./Product.module.css";
 
+/* eslint react/prop-types: 0 */
+function Provider({ name, href }) {
+  const style = {
+    textDecoration: "inherit",
+    color: "inherit",
+  };
+
+  return (
+    <a href={href} rel="noopener noreferrer" target="_blank" style={style}>
+      <li>{name}</li>
+    </a>
+  );
+}
+
+const providers = [
+  { name: "👉 AWS Amplify", href: "https://aws.amazon.com/amplify" },
+  { name: "👉 React 18", href: "https://react.dev/" },
+  { name: "👉 Vite", href: "https://vitejs.dev/" },
+  { name: "👉 Pockethost.io", href: "https://pockethost.io/" },
+  { name: "👉 Leaflet", href: "https://leafletjs.com/" },
+  { name: "👉 CKEditor", href: "https://ckeditor.com/" },
+];
+
 export default function Product() {
   return (
     <main className={styles.product}>
@@ -15,11 +38,9 @@ export default function Product() {
           <h2>About GeoNotes.</h2>
           <p>GeoNotes is powered by:</p>
           <ul style={{ fontSize: "15px", listStyleType: "none" }}>
-            <li>👉 AWS Amplify</li>
-            <li>👉 React & Vite</li>
-            <li>👉 Pockethost.io</li>
-            <li>👉 Leaflet</li>
-            <li>👉 CKEditor</li>
+            {providers.map((provider, index) => (
+              <Provider key={index} {...provider} />
+            ))}
           </ul>
         </div>
       </section>
