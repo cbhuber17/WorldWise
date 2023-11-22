2851dd7a48f046231628ae80a3d277470f3dc66b
Colin Huber <cbhuber@gmail.com>
2023-11-22 14:18:06 -0700

    feat: pricing page responsiveness.

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -16,27 +16,21 @@ export default function Product() {
           <br />
           <p>
             I believe in making travel experiences accessible to all, which is
-            why our travel journal web app is free. We want to encourage people
+            why the travel journal web app is free. I want to encourage people
             from all walks of life to document and cherish their journeys
             without any financial barriers.
-          </p>
-          <p>
-            My mission is to inspire wanderlust and connect travelers through
-            the joy of sharing their adventures. Keeping our travel journal web
-            app free ensures that everyone can participate and share their
-            unique travel stories.
-          </p>
-          <p>
-            I am passionate about fostering a global community of explorers, and
-            offering our app for free is our way of giving back to the travel
-            community and helping travelers preserve their precious memories at
-            no cost.
           </p>
         </div>
         <img
           // src="/assets/img/img-2.jpg"
           src="/WorldWise/assets/img/img-2.jpg"
           alt="overview of a large city with skyscrapers"
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            width: "auto",
+            height: "auto",
+          }}
         />
       </section>
     </main>
