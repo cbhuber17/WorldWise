ea6eddb34d6b0b0b75dea29ef9f81ec2982cae3f
Colin Huber <cbhuber@gmail.com>
2023-11-24 11:52:55 -0700

    fix: image url.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,8 +3,8 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <img
-      src="/assets/img/logo.png"
-      // src="/WorldWise/assets/img/logo.png"
+      // src="/assets/img/logo.png"
+      src="/WorldWise/assets/img/logo.png"
       alt="GeoNotes logo"
       className={styles.logo}
     />
